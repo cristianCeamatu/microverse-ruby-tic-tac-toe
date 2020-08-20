8db8afb6e019385804003dcd173152cee3697f77
cristianCeamatu <cristian.ceamatu@gmail.com>
2020-08-20 16:10:15 +0300

    Utils methods tests

--- lib/utils.rb
+++ lib/utils.rb
@@ -1,7 +1,7 @@
 module Utils
   def validate_name(input)
     return 'Only String type accepted for the input' unless input.is_a?(String)
-    return 'Only alphabet letters accepted (no numbers or special characters)' unless input.match(/([a-zA-Z]+)/)
+    return 'Only alphabet letters accepted (no numbers or special characters)' unless input.match(/^[A-Z]+$/i)
     return 'The length of the input should be between 3-16 letters' unless input.strip.length.between?(3, 16)
 
     input.strip
